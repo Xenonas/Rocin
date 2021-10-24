647dfe6be8711b83849b9dc17763c9e2a5144836
= <=>
2021-10-24 14:14:55 +0300

    2d rotations simple

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://rocin-rails.herokuapp.com/'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', 'https://rocin-rails.herokuapp.com/'),
 
